5b209afe6ce24254892102c730e7c4cd3a710502
Tim Burke <tim.burke@gmail.com>
2016-07-05 10:45:45 -0700

    Drop support for pre-2.5 Python
    Change-Id: Ic48836b3334d32f8ccd3e77482c2e021b452c6c3

--- lodgeit/lib/xmlrpc.py
+++ lodgeit/lib/xmlrpc.py
@@ -8,7 +8,6 @@
     :copyright: 2007 by Armin Ronacher.
     :license: BSD
 """
-import sys
 import re
 from SimpleXMLRPCServer import SimpleXMLRPCDispatcher
 from werkzeug import Response
@@ -21,12 +20,7 @@ _strip_re = re.compile(r'[\x00-\x08\x0B-\x1F]')
 class XMLRPCRequestHandler(SimpleXMLRPCDispatcher):
 
     def __init__(self):
-        #: Python 2.5 requires some arguments like `allow_none`
-        #: and the encoding. Python 2.4 and 2.3 doesn't.
-        if sys.version_info[:2] < (2, 5):
-            SimpleXMLRPCDispatcher.__init__(self)
-        else:
-            SimpleXMLRPCDispatcher.__init__(self, True, 'utf-8')
+        SimpleXMLRPCDispatcher.__init__(self, True, 'utf-8')
 
     def handle_request(self):
         def dispatch(method_name, params):
