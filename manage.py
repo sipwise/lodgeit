b7a9033b494c49aed76abaf5187d2a3f5870b226
Sergii Kipot <kipot.sergey@gmail.com>
2019-11-07 16:37:47 +0100

    Redesign manage.py to not use deprecated werkzeug.script
    werkzeug.script is deprecated long time ago and manage.py fails with
    python-werkzeug >=0.13. Let's use werkzeug.serving and python-click
    instead.
    
    Also add import for xrange otherwise tests fail:
    ./lodgeit/lib/captcha.py:222:22: F821 undefined name 'xrange'
                for i in xrange(-1, int(image.size[0] / tile.size[0]) + 1):
    
    Change-Id: I6dd17a78b3b6beac987f46542c6a2e7a0a5939bb
    Signed-off-by: Sergii Kipot <kipot.sergey@gmail.com>

--- manage.py
+++ manage.py
@@ -1,38 +1,80 @@
 #!/usr/bin/env python
 
-from werkzeug import script, create_environ, run_wsgi_app
-
+import click
+from werkzeug.serving import run_simple
+import lodgeit.application
 from lodgeit import local
-from lodgeit.application import make_app
 from lodgeit.database import db
 
 dburi = 'sqlite:////tmp/lodgeit.db'
 
 SECRET_KEY = 'no secret key'
 
+def make_app():
+    return lodgeit.application.make_app(dburi, SECRET_KEY, debug=False)
 
-def run_app(app, path='/'):
-    env = create_environ(path, SECRET_KEY)
-    return run_wsgi_app(app, env)
+def make_shell():
+    from lodgeit import local
+    from lodgeit.database import db
 
+    application = make_app()
+    return {"application": application, "local": local, "db": db}
 
-action_runserver = script.make_runserver(
-    lambda: make_app(dburi, SECRET_KEY, debug=True),
-    use_reloader=True)
+@click.group()
+def cli():
+    pass
 
+@cli.command()
+@click.option("-h", "--hostname", type=str, default="localhost", help="localhost")
+@click.option("-p", "--port", type=int, default=5000, help="5000")
+@click.option("--no-reloader", is_flag=True, default=False)
+@click.option("--debugger", is_flag=True)
+@click.option("--no-evalex", is_flag=True, default=False)
+@click.option("--threaded", is_flag=True)
+@click.option("--processes", type=int, default=1, help="1")
+def runserver(hostname, port, no_reloader, debugger, no_evalex, threaded, processes):
+    """Start a new server server."""
+    app = make_app()
+    reloader = not no_reloader
+    evalex = not no_evalex
+    run_simple(
+        hostname,
+        port,
+        app,
+        use_reloader=reloader,
+        use_debugger=debugger,
+        use_evalex=evalex,
+        threaded=threaded,
+        processes=processes,
+    )
 
-action_shell = script.make_shell(
-    lambda: {
-        'app': make_app(dburi, SECRET_KEY, False, True),
-        'local': local,
-        'db': db,
-        'run_app': run_app
-    },
-    ('\nWelcome to the interactive shell environment of LodgeIt!\n'
+@cli.command()
+@click.option("--no-ipython", is_flag=True, default=False)
+def shell(no_ipython):
+    """Start a new interactive python session."""
+    banner = ('\nWelcome to the interactive shell environment of LodgeIt!\n'
      '\n'
      'You can use the following predefined objects: app, local, db.\n'
      'To run the application (creates a request) use *run_app*.')
-)
+    namespace = make_shell()
+    if not no_ipython:
+        try:
+            try:
+                from IPython.frontend.terminal.embed import InteractiveShellEmbed
 
-if __name__ == '__main__':
-    script.run()
+                sh = InteractiveShellEmbed.instance(banner1=banner)
+            except ImportError:
+                from IPython.Shell import IPShellEmbed
+
+                sh = IPShellEmbed(banner=banner)
+        except ImportError:
+            pass
+        else:
+            sh(local_ns=namespace)
+            return
+    from code import interact
+
+    interact(banner, local=namespace)
+
+if __name__ == "__main__":
+    cli()
